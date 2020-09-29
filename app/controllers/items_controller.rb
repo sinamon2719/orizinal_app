f95f78cd4d9e50012a71f291980f29215f11c89c
sinamon2719 <okigeisai2012@yahoo.co.jo>
2020-09-29 15:34:21 +0900

    新規登録画面　リンク実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,18 @@ class ItemsController < ApplicationController
   def index
     
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
