da864fd3c31cad227eccf83b09ecdbb72d3fbe71
sinamon2719 <okigeisai2012@yahoo.co.jo>
2020-09-30 22:04:27 +0900

    9/30　最新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
 
   def index
-    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -18,6 +20,26 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  def update
+    if @item.user_id == current_user.id
+      if @item.update(item_params)
+        redirect_to item_path(@item.id)
+      else
+        redirect_to action: :edit
+      end
+    else
+      redirect_to action: :edit
+    end
+  end
+
   def show
     @items = Item.includes(:user).order('created_at DESC')
     @item = Item.new(item_params)
@@ -26,7 +48,10 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.permit(:image, :name, :content, :category_id, :shipping_cost_id, :shipping_day_id, :prefecture_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :content, :category_id, :shipping_cost_id, :shipping_day_id, :prefecture_id, :price).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  get 'items/show'
   root to: 'items#index'
   resources :users
   resources :items
